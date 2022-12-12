21d3ba6c00e6385f70b35cbdd74685f38d786a51
elodie <e.fontaine.1991@hotmail.fr>
2022-12-12 15:16:38 +0100

    ajout des view

---
+++ src/app/functionalitys-list/functionalitys-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FunctionalitysListComponent } from './functionalitys-list.component';
+
+describe('FunctionalitysListComponent', () => {
+  let component: FunctionalitysListComponent;
+  let fixture: ComponentFixture<FunctionalitysListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FunctionalitysListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FunctionalitysListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/functionalitys-list/functionalitys-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-functionalitys-list',
+  templateUrl: './functionalitys-list.component.html',
+  styleUrls: ['./functionalitys-list.component.scss']
+})
+export class FunctionalitysListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
