64ccfb10ee63017dc14cc20cbb5ff6db58d2ce1d
elodie <e.fontaine.1991@hotmail.fr>
2022-12-12 15:14:16 +0100

    initialisation projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { FunctionalitysListComponent } from './functionalitys-list/functionalitys-list.component';
+import { ComponentsListComponent } from './components-list/components-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsListComponent,
+    FunctionalitysListComponent,
+    ComponentsListComponent
   ],
   imports: [
     BrowserModule,
