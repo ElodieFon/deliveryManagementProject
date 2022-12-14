3fc7e410a089357218672c8fc2f1321777680d72
elodie <e.fontaine.1991@hotmail.fr>
2022-12-14 14:43:30 +0100

    config route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Location } from '@angular/common'
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
+import { ProjectSingleComponent } from './components/project-single/project-single.component';
+import { FunctionalitySingleComponent } from './components/functionality-single/functionality-single.component';
+import { ComponentSingleComponent } from './components/functionalityComponent-single/component-single.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'projects', component:ProjectsListComponent},
+  {path: 'project/:projectId',component:ProjectSingleComponent},
+  {path: 'functionality/:functionalityId',component:FunctionalitySingleComponent},
+  {path: 'component/:componentId',component:ComponentSingleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  constructor(private location: Location){}
+  
+  back(): void {
+    this.location.back()
+  }
+}
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import{ Observable, of}from 'rxjs';
+
+import { Project } from '../models/Project';
+import { PROJETS } from '../mock-projects';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,13 @@ import { Injectable } from '@angular/core';
 export class ProjectService {
 
   constructor() { }
+
+  getAllProjects(){
+    const allProjects = of(PROJETS);
+    return allProjects ; 
+  }
+  getProjectById(idProject : number){
+    const project = PROJETS.find(p => p.id === idProject)!;
+    return project;
+  }
 }
