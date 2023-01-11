b7afb26f00505018704a13f198e18702012929b5
elodie <e.fontaine.1991@hotmail.fr>
2023-01-11 12:51:39 +0100

    ajout de l'imort du client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProjectsListComponent} from './components/projects-list/projects-list.component';
 import { ProjectSingleComponent } from './components/project-single/project-single.component';
 import { FunctionalitySingleComponent } from './components/functionality-single/functionality-single.component';
 import { ComponentSingleComponent } from './components/functionalityComponent-single/component-single.component';
+
 
 
 
@@ -19,10 +21,12 @@ import { ComponentSingleComponent } from './components/functionalityComponent-si
     ProjectsListComponent,
     FunctionalitySingleComponent,
     ComponentSingleComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
